704258823c4f4502ca9b84af0e3848ed0a5faccb
wonseokyoon <backend.wonseok@gmail.com>
2025-04-17 12:08:11 +0900

    init

---
+++ src/test/kotlin/com/terra/TerraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.terra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TerraApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
